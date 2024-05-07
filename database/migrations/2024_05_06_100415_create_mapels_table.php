89605b94bb2a5e7d2f6f278406c46fafd3d2eaed
rizkyfdlh <167829205+rizkyfdlh@users.noreply.github.com>
2024-05-07 13:19:36 +0700

    Add files via upload

---
+++ database/migrations/2024_05_06_100415_create_mapels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mapels', function (Blueprint $table) {
+            $table->id();
+            $table->string('matematika');
+            $table->string('penjaskes');
+            $table->string('informatika');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mapels');
+    }
+};
---
+++ database/migrations/2024_05_06_101410_create_jabatans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jabatans', function (Blueprint $table) {
+            $table->id();
+            $table->string('kepala_sekolah');
+            $table->string('kesiswaan');
+            $table->string('hubin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jabatans');
+    }
+};
